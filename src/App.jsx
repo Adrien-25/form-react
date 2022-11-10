5aa2e362e1c1a69baeabd6597a25928d4346f0dc
Adrien <adrien.schmidt7@gmail.com>
2022-11-10 11:21:40 +0100

    Ajout du formulaire de connexion

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Route, Routes} from 'react-router-dom';
 import Counter from './pages/Counter';
 import Header from './components/Header';
 import UserList from './pages/UserList';
-import SimLogin from './pages/SimLogin';
+import Login from './pages/Login';
 import './App.css';
 import propTypes from 'prop-types';
 import Roles from './pages/Roles'
@@ -21,7 +21,7 @@ function App() {
           <Routes>
             <Route path="/" element={<UserList />}/>
             <Route path="/counter" element={<Counter />}/>
-            <Route path="/login" element={<SimLogin setUser={setUser} />}/>
+            <Route path="/login" element={<Login setUser={setUser} />}/>
             <Route path="/roles" element={<Roles />}/>
             <Route path="/register" element={<Register setUser={setUser} />}/>
           </Routes>
